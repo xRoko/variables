18f2c57f5b4d8d9370d189496078c3cfe8a0a6ab
mkanuch <mkanuch@gmail.com>
2024-12-02 20:20:34 +0100

    aktualizovany kod podla tvojho tipu

--- birth.js
+++ birth.js
@@ -1,8 +1,9 @@
 const prompt = require('prompt-sync')();
 
-const username = prompt ('What is your name?');
+const username = prompt ('What is your name? ');
 
 let userage = prompt ('Please, write your age here: ');
-const year = 2024 - userage;
 
-console.log(`Hey "${username}", you were born in ${year}!`);
+console.log('')
+
+console.log(`Hey "${username}", you were born in ${2024 - userage}!`);
