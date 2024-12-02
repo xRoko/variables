18dd0824e3eeff442bab27bb77ecf0f3ccff5cf5
Andrej2610 <andrejapyro@seznam.cz>
2024-12-02 10:44:16 +0100

    Domaci ulohy

--- rect.js
+++ rect.js
@@ -6,6 +6,15 @@
 
 const prompt = require('prompt-sync')();
 
+let delka = Number(prompt('delka obdelnika: '));
+let sirka = Number(prompt('sirka obdelnika: '));
+
+let obvod = (2 * (delka + sirka));
+let obsah = (delka * sirka);
+
+
+console.log(`Obvod: ${obvod}, Obsah: ${obsah}`);
+
 /*
     EXAMPLE USAGE
     const name = prompt('What is your name?');
